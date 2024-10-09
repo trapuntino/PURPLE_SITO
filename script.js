12d2cd0e6456f77c59bc873c04eee2f339a95790
trapuntino <71377169+trapuntino@users.noreply.github.com>
2024-10-09 11:54:44 +0200

    LOADER

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Script is running!");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loadingScreen = document.getElementById('loading-screen');
+    const mainContent = document.getElementById('main-content');
+    const loadingVideo = document.getElementById('loading-video');
+
+    // Hide loading screen and show main content after video ends
+    loadingVideo.addEventListener('ended', () => {
+        loadingScreen.style.display = 'none';  // Hide loading screen
+        mainContent.style.display = 'block';  // Show main content
+    });
+});
